bbafe1ed9fe222242e17fe9da915e05286f1bfa6
Luis Felipe <luisfelipepereira150@gmail.com>
2020-11-13 01:28:33 -0300

    Creation initial the screens

---
+++ app/src/main/java/com/example/marvelandroid/activity/ViewDetailsCreator.java
@@ -0,0 +1,16 @@
+package com.example.marvelandroid.activity;
+
+import android.os.Bundle;
+
+import com.example.marvelandroid.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewDetailsCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_creator);
+    }
+}
